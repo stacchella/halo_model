c13b2003ce2da5205630b07ea51a8f6d4e54139d
stacchella <sandro.tacchella@cfa.harvard.edu>
2017-12-21 14:56:28 -0500

    added cluster submission scripts

---
+++ scripts/cluster_runs/submission_script_make_SFH.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p general
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### Job name
+#SBATCH -J 'random_SFH'
+### output and error logs
+#SBATCH -o random_SFH_%a.out
+#SBATCH -e random_SFH_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
+source activate pro
+srun -n 1 python /n/regal/eisenstein_lab/stacchella/halo_model/scripts/run_make_SFH.py
+
