4f3c4426f2dd5c6b2178c2d798d59bd3976804a4
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-01-02 13:26:22 -0500

    updated cluster run script

--- scripts/cluster_runs/submission_script_make_SFH.sh
+++ scripts/cluster_runs/submission_script_make_SFH.sh
@@ -20,5 +20,5 @@
 #SBATCH --mail-user=sandro.tacchella@cfa.harvard.edu
 source activate pro
 srun -n 1 python /n/regal/eisenstein_lab/stacchella/halo_model/scripts/run_make_SFH.py \
---number_of_bins=20 \
+--number_of_bins=200 \
 --idx_halo_key="${SLURM_ARRAY_TASK_ID}"
