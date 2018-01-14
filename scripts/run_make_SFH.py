87cb678a3170d3d2b6525e6a8c5fe06ddc0f72eb
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-01-13 20:25:37 -0500

    updated and added epsilon

--- scripts/run_make_SFH.py
+++ scripts/run_make_SFH.py
@@ -29,21 +29,25 @@ path_SFH_cat = path_main + 'catalogs/SFH/'
 
 # set parameters
 
-efficency_filename = 'calibration/epsilon.npy'
-SFH_type_option = 'random'
+SFH_type_option = 'constant'  # 'constant' or 'random'
+efficency_filename = 'calibration/epsilon_' + SFH_type_option + '.npy'
 
 # z=4
-# DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z3.96.hdf5'
+DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z3.96.hdf5'
 # filename_SFH_file = 'SFH_z4_random.hdf5'
+filename_SFH_file = 'SFH_z4_const.hdf5'
 # z=6
-DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z5.98.hdf5'
-filename_SFH_file = 'SFH_z6_random.hdf5'
+# DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z5.98.hdf5'
+# filename_SFH_file = 'SFH_z6_random.hdf5'
+# filename_SFH_file = 'SFH_z6_const.hdf5'
 # z=8
 # DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z8.10.hdf5'
 # filename_SFH_file = 'SFH_z8_random.hdf5'
+# filename_SFH_file = 'SFH_z8_const.hdf5'
 # z=10
 # DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z10.00.hdf5'
 # filename_SFH_file = 'SFH_z10_random.hdf5'
+# filename_SFH_file = 'SFH_z10_const.hdf5'
 
 
 # read in command line arguments
