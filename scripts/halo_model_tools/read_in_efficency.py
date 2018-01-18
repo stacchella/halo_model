77916788deec209e9627888be40d6320a98d53d1
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-01-18 10:10:55 -0700

    updated epsilon function (median)

--- scripts/halo_model_tools/read_in_efficency.py
+++ scripts/halo_model_tools/read_in_efficency.py
@@ -4,7 +4,6 @@ December 20, 2017
 '''
 
 import numpy as np
-import h5py
 
 
 def read_in_efficency(file_name):
@@ -12,11 +11,9 @@ def read_in_efficency(file_name):
     Reads in and manipulates dark matter halo accretion history.
     '''
     # read in catalog
-
     epsilon_info = np.load(file_name)
-    epsilon_info_Mh = epsilon_info.T[0][1:]
-    epsilon_info_epsi = epsilon_info[0][1:]
-    epsilon_matrix = epsilon_info[1:, 1:]
+    epsilon_info_Mh = epsilon_info[0]
+    epsilon_info_epsi = epsilon_info[1]
     # define epsilon function
     def draw_epsilon(Mh_in, size_in=1.0):
         '''
@@ -24,8 +21,30 @@ def read_in_efficency(file_name):
         the calibrated distribution for a given halo mass.
         '''
         idx_Mh = np.abs(epsilon_info_Mh-Mh_in).argmin()
-        epsilon_distribution = epsilon_matrix[idx_Mh]
-        return(np.random.choice(epsilon_info_epsi, size=size_in, replace=True, p=epsilon_distribution/np.sum(epsilon_distribution))) 
+        epsilon_median = epsilon_info_epsi[idx_Mh]
+        return(epsilon_median*np.ones(size_in))
     return(draw_epsilon)
+
+
+# def read_in_efficency(file_name):
+#     '''
+#     Reads in and manipulates dark matter halo accretion history.
+#     '''
+#     # read in catalog
+
+#     epsilon_info = np.load(file_name)
+#     epsilon_info_Mh = epsilon_info.T[0][1:]
+#     epsilon_info_epsi = epsilon_info[0][1:]
+#     epsilon_matrix = epsilon_info[1:, 1:]
+#     # define epsilon function
+#     def draw_epsilon(Mh_in, size_in=1.0):
+#         '''
+#         This function returns an efficency from
+#         the calibrated distribution for a given halo mass.
+#         '''
+#         idx_Mh = np.abs(epsilon_info_Mh-Mh_in).argmin()
+#         epsilon_distribution = epsilon_matrix[idx_Mh]
+#         return(np.random.choice(epsilon_info_epsi, size=size_in, replace=True, p=epsilon_distribution/np.sum(epsilon_distribution))) 
+#     return(draw_epsilon)
 
 
