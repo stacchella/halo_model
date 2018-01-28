44b76767662db18ccb45c37a880ae29da174de57
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-01-28 08:33:19 -0500

    updated redshift

--- scripts/summarize_all_SFH_runs.py
+++ scripts/summarize_all_SFH_runs.py
@@ -31,12 +31,12 @@ path_SFH_cat = path_main + 'catalogs/SFH/'
 SFH_type_option = 'constant'  # 'constant' or 'random'
 
 # z=4
-DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z3.96.hdf5'
-filename_SFH_file = 'SFH_z4_' + SFH_type_option + '.hdf5'
+# DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z3.96.hdf5'
+# filename_SFH_file = 'SFH_z4_' + SFH_type_option + '.hdf5'
 # filename_SFH_file = 'SFH_z4_' + SFH_type_option + '_calibration.hdf5'
 # z=6
-# DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z5.98.hdf5'
-# filename_SFH_file = 'SFH_z6_' + SFH_type_option + '.hdf5'
+DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z5.98.hdf5'
+filename_SFH_file = 'SFH_z6_' + SFH_type_option + '.hdf5'
 # z=8
 # DM_accretion_history_filename = 'MergerHistory_COLOR_CDM_z8.10.hdf5'
 # filename_SFH_file = 'SFH_z8_' + SFH_type_option + '.hdf5'
