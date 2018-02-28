312a2f76dea584ae1c495acc28574f184d4bd1d7
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-02-27 22:01:13 -0500

    major updated computation SP

--- scripts/halo_model_tools/derive_SP_prop.py
+++ scripts/halo_model_tools/derive_SP_prop.py
@@ -5,47 +5,25 @@ December 21, 2017
 
 import numpy as np
 
-
-def get_UVandHa_lum(sp_in, age_in):
-    '''
-    Function computes UV luminosities from model sp.
-    - sp:     stellar population model
-    - age_in: age in Gyr
-    '''
-    # get UV magnitudes
-    UV_mags = sp_in.get_mags(tage=age_in, bands=['i1500', 'i2300', 'i2800'])
-    # convert AB mags to luminosity
-    flux_1500 = np.power(10, -0.4*(UV_mags[0]+48.6))
-    L_1500 = flux_1500*4*np.pi*(3.086e+19)**2
-    flux_2300 = np.power(10, -0.4*(UV_mags[1]+48.6))
-    L_2300 = flux_2300*4*np.pi*(3.086e+19)**2
-    flux_2800 = np.power(10, -0.4*(UV_mags[2]+48.6))
-    L_2800 = flux_2800*4*np.pi*(3.086e+19)**2
-    # get Halpha luminosity
-    idx_Halpha = (np.abs(sp_in.emline_wavelengths-6564.61)).argmin()
-    idx_Hbeta = (np.abs(sp_in.emline_wavelengths-4862.68)).argmin()
-    L_Ha = 3.839*10**33*sp_in.emline_luminosity[idx_Halpha]
-    L_Hb = 3.839*10**33*sp_in.emline_luminosity[idx_Hbeta]
-    return(L_1500, L_2300, L_2800, L_Ha, L_Hb)
-
-
-def get_luminosities_for_SFH(sp_in, SFH_in, age_list):
+def get_luminosities_for_SFH(sp_in, SFH_in, age_in, idx_EL, wave_interpolate):
     '''
     Function computes Ha and UV luminosities for certain SFH.
     - SFH_in    : 2xN array with age and SFR
-    - age_list  : list of ages for which UV and Ha luminosities should be computed
-                  in Gyr
+    - age_in    : age where compute spectrum, in Gyr
     '''
     # set SFH
     sp_in.set_tabular_sfh(SFH_in[0], SFH_in[1], Z=None)
-    lum_1500, lum_2300, lum_2800, lum_Ha, lum_Hb = [], [], [], [], []
-    for ii_age in age_list:
-        #print 'progress [in %]: ', np.round(100.0*np.sum(age_list<=ii_age)/len(age_list))
-        lum = get_UVandHa_lum(sp_in, 0.999*ii_age)
-        lum_1500 = np.append(lum_1500, lum[0])
-        lum_2300 = np.append(lum_2300, lum[1])
-        lum_2800 = np.append(lum_2800, lum[2])
-        lum_Ha = np.append(lum_Ha, lum[3])
-        lum_Hb = np.append(lum_Hb, lum[4])
-    return(lum_Ha, lum_Hb, lum_1500, lum_2300, lum_2800)
+    # get mass live
+    stellar_mass = sp_in.stellar_mass
+    # get magnitudes from spectraum WITH emission lines
+    sp_in.params['nebemlineinspec'] = True
+    mag_list = sp_in.get_mags(tage=age_in, bands=['i1500', 'i2300', 'i2800', 'v', 'u', '2mass_j'])
+    L_filters_list = np.power(10, -0.4*(mag_list+48.6))*4*np.pi*(3.086e+19)**2
+    # get emission lines
+    L_EL_list = 3.839*10**33*sp_in.emline_luminosity[idx_EL]
+    # downgrade spectrum without emission lines
+    sp_in.params['nebemlineinspec'] = False
+    wave, spec = sp_in.get_spectrum(tage=age_in)
+    spec_interpolate = np.interp(wave_interpolate, wave, spec)
+    return(stellar_mass, L_filters_list, L_EL_list, spec_interpolate)
 
--- scripts/run_derive_SP.py
+++ scripts/run_derive_SP.py
@@ -51,6 +51,15 @@ path_SP_cat = path_main + 'catalogs/SP/'
 idx_every_other = 2  # fundge factor so that SFH setting works
 SFH_type_option = 'constant'  # 'constant' or 'random'
 
+# emission lines to be saved
+wl_EL = np.array([1.215670e+03, 1.640420e+03, 1.661240e+03, 1.666150e+03, 1.906680e+03, 1.908730e+03, 1.908730e+03, 3.727100e+03, 4.862710e+03, 5.008240e+03, 6.564600e+03, 6.585270e+03, 6.718290e+03, 6.732670e+03])
+
+# wavelength grid
+R = 300.0
+w_min = 3500.0
+w_max = 100000.0
+
+
 # z=4
 # filename_SFH_file = 'SFH_z4_' + SFH_type_option + '.hdf5'
 # filename_SP_file = 'SFH_z4_' + SFH_type_option + '_with_L.hdf5'
@@ -63,8 +72,8 @@ SFH_type_option = 'constant'  # 'constant' or 'random'
 # filename_SFH_file = 'SFH_z8_' + SFH_type_option + '.hdf5'
 # filename_SP_file = 'SFH_z8_' + SFH_type_option + '_with_L.hdf5'
 # z=10
-filename_SFH_file = 'SFH_z10_' + SFH_type_option + '.hdf5'
-filename_SP_file = 'SFH_z10_' + SFH_type_option + '_with_L.hdf5'
+# filename_SFH_file = 'SFH_z10_' + SFH_type_option + '.hdf5'
+# filename_SP_file = 'SFH_z10_' + SFH_type_option + '_with_L.hdf5'
 
 
 # read in command line arguments
@@ -72,17 +81,21 @@ filename_SP_file = 'SFH_z10_' + SFH_type_option + '_with_L.hdf5'
 parser = argparse.ArgumentParser()
 parser.add_argument("--number_of_bins", type=int, help="number of cores")
 parser.add_argument("--idx_halo_key", type=int, help="iteration variable")
+parser.add_argument("--filename_SFH", type=int, help="filename of SFH file")
+parser.add_argument("--filename_SP", type=int, help="filename of SP file")
 args = parser.parse_args()
 
 
 run_params = {'number_of_bins': args.number_of_bins,  # this gives number of cores we run on
               'idx_halo_key': args.idx_halo_key,  # iteration variable
+              'filename_SFH': args.filename_SFH,  # filename of SFH file
+              'filename_SP': args.filename_SP,  # filename of SP file
               }
 
 
 # read in SFH
 
-SFH_file = h5py.File(path_SFH_cat + filename_SFH_file, 'r')
+SFH_file = h5py.File(path_SFH_cat + args.filename_SFH, 'r')
 SFH_time = SFH_file['SFH/SFH_time'][:]
 SFH_SFR = SFH_file['SFH/SFH_SFR'][:]
 
@@ -90,15 +103,21 @@ SFH_SFR = SFH_file['SFH/SFH_SFR'][:]
 # set up new hdf5 file for saving luminosities optained from SFH
 
 try:
-    os.remove(path_SP_cat + '/' + filename_SFH_file[:-5] + '/' + filename_SP_file[:-5] + '_' + str(int(float(args.idx_halo_key))-1) + '.hdf5')
+    os.remove(path_SP_cat + '/' + args.filename_SFH[:-5] + '/' + args.filename_SP[:-5] + '_' + str(int(float(args.idx_halo_key))-1) + '.hdf5')
 except OSError:
     pass
 
 
-lum_file = h5py.File(path_SP_cat + '/' + filename_SFH_file[:-5] + '/' + filename_SP_file[:-5] + '_' + str(int(float(args.idx_halo_key))-1) + '.hdf5', 'w')
+lum_file = h5py.File(path_SP_cat + '/' + args.filename_SFH[:-5] + '/' + args.filename_SP[:-5] + '_' + str(int(float(args.idx_halo_key))-1) + '.hdf5', 'w')
 # add luminosities
-grp_lum = lum_file.create_group("luminosities")
-grp_lum.attrs['luminosity_info'] = 'L_Ha', 'L_Hb', 'L_1500', 'L_2300', 'L_2800'
+grp_lum = lum_file.create_group("SP")
+grp_EmL_lum = grp_lum.create_group("EmL")
+grp_EmL_lum.attrs['EL_info'] = 'L_Lya', 'L_HeII', 'L_OIII_L1', 'L_OIII_L2', 'L_CIII_1', 'L_CIII_2', 'L_CIV', 'L_OII', 'L_Hb', 'L_OIII', 'L_Ha', 'L_NII', 'L_SII_1', 'L_SII_2'
+grp_EmL_lum.attrs['EL_wavelength'] = wl_EL
+grp_FilL_lum = grp_lum.create_group("FilL")
+grp_FilL_lum.attrs['FL_info'] = 'i1500', 'i2300', 'i2800', 'v', 'u', '2mass_j'
+grp_spec_lum = grp_lum.create_group("spec")
+
 
 # close other (SFH) hdf5 file
 SFH_file.close()
@@ -106,8 +125,8 @@ SFH_file.close()
 
 # set up the grid
 
-logzsol_grid = [0.0, -0.5, -1.0, -1.5, -2.0]
-IMF_grid = [0, 1, 3]
+logzsol_grid = [0.0, -0.7, -1.0, -1.7, -2.0]
+IMF_grid = [0, 1]
 
 iterables = [logzsol_grid, IMF_grid]
 
@@ -168,29 +187,47 @@ for ii_model in range(len(dict_all_combinations)):
     print '                   IMF        = ', model_dict['IMF_name']
     # set standard SP model
     # here we set metallicity and IMF
-    sp_now = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=1, imf_type=model_dict['IMF'], add_neb_emission=True, sfh=3, logzsol=model_dict['logzsol'], dust_type=2, dust2=0.0)
+    sp_now = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=1, imf_type=model_dict['IMF'], add_neb_emission=True, sfh=3, logzsol=model_dict['logzsol'], dust_type=2, dust2=0.0, sigma_smooth=1000.0, min_wave_smooth=3525.0, max_wave_smooth=7500.0)
+    # age of stellar population (age of the Universe at current redshift), in Gyr
+    tage_now = 10**-3*SFH_time[::idx_every_other][-1]
+    # set up index array for emission lines
+    idx_EL = []
+    for ii_wl in wl_EL:
+        idx_EL.append(int((np.abs(sp_now.emline_wavelengths-ii_wl)).argmin()))
+    # get wavelength array for interpolation
+    wave, spec = sp_now.get_spectrum(tage=tage_now)
+    wavelength_interpolate = [w_min]
+    while (wavelength_interpolate[-1] < w_max):
+        wavelength_interpolate.append(wavelength_interpolate[-1]*(1.0+1.0/R))
+    wavelength_interpolate = np.append(wave[wave < w_min], wavelength_interpolate)
+    wavelength_interpolate = np.append(wavelength_interpolate, wave[wave > w_max])
     # iterate over all SFHs
     c = 0
     for ii in idx_halo_considered:  # range(SFH_SFR.shape[0]):
         print 'progress (%): ', round(100.0*c/len(idx_halo_considered), 3)
         c += 1
-        lum_vec = derive_SP_prop.get_luminosities_for_SFH(sp_now, [10**-3*SFH_time[::idx_every_other], SFH_SFR[ii][::idx_every_other]], [10**-3*SFH_time[::idx_every_other][-1]])
-        lum_vec = np.array(lum_vec).T
+        stellar_mass, L_filters_list, L_EL_list, spec_interpolate = derive_SP_prop.get_luminosities_for_SFH(sp_now, [10**-3*SFH_time[::idx_every_other], SFH_SFR[ii][::idx_every_other]], tage_now, idx_EL, wavelength_interpolate)
         if (ii == idx_halo_considered[0]):
-            lum_mat = lum_vec
+            stellar_mass_list = stellar_mass
+            L_filters_mat = np.array(L_filters_list).T
+            L_EL_mat = np.array(L_EL_list).T
+            spec_interpolate_mat = np.array(spec_interpolate).T
         else:
-            lum_mat = np.vstack([lum_mat, lum_vec])
-    # add SFH
-    subgrp_lum = grp_lum.create_dataset('luminosity_' + str(ii_model), data=lum_mat)
+            stellar_mass_list = np.append(stellar_mass_list, stellar_mass)
+            L_filters_mat = np.vstack([L_filters_mat, np.array(L_filters_list).T])
+            L_EL_mat = np.vstack([L_EL_mat, np.array(L_EL_list).T])
+            spec_interpolate_mat = np.vstack([spec_interpolate_mat, np.array(spec_interpolate).T])
+    # add to hdf5 file
+    if (ii_model == 0):
+        subgrp_lum = grp_lum.create_dataset('stellar_mass', data=stellar_mass_list)
+    subgrp_FilL_lum = grp_FilL_lum.create_dataset('luminosity_' + str(ii_model), data=L_filters_mat)
+    subgrp_EmL_lum = grp_EmL_lum.create_dataset('luminosity_' + str(ii_model), data=L_EL_mat)
+    subgrp_spec_lum = grp_spec_lum.create_dataset('luminosity_' + str(ii_model), data=spec_interpolate_mat)
     for ii_key in model_dict.keys():
-        subgrp_lum.attrs[ii_key] = model_dict[ii_key]
+        subgrp_FilL_lum.attrs[ii_key] = model_dict[ii_key]
+        subgrp_EmL_lum.attrs[ii_key] = model_dict[ii_key]
+        subgrp_spec_lum.attrs[ii_key] = model_dict[ii_key]
 
 
 lum_file.close()
-
-
-
-
-
-
 
