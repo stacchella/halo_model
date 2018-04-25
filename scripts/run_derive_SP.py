aa1ee94b3ff32e905263c464449531c1e40303b6
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-04-25 09:26:23 -0400

    updated SP grid

--- scripts/run_derive_SP.py
+++ scripts/run_derive_SP.py
@@ -110,10 +110,10 @@ SFH_file.close()
 # set up the grid
 
 if (args.calibration_run == 'True'):
-    logzsol_grid = [-1.0, -1.7, -3.0, -99.0]
+    logzsol_grid = [-1.7, -99.0]
     IMF_grid = [0]
 else:
-    logzsol_grid = [0.0, -1.0, -1.7, -2.0, -2.7, -3.0, -99.0]
+    logzsol_grid = [0.0, -1.0, -1.7, -3.0, -99.0]
     IMF_grid = [0, 1]
 
 iterables = [logzsol_grid, IMF_grid]
